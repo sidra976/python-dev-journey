e91e6ac5d3ef51ab46c15c4aec9692361700ee65
sidra976 <sidraahmed4477@gmail.com>
2025-06-09 22:12:06 +0500

    add

--- 01_basics/loops.py
+++ 01_basics/loops.py
@@ -10,3 +10,11 @@ for x in numbers:
         count_positive_int = count_positive_int+1
 
 print(count_positive_int)        
+
+num = [1,2,3,4,5,6,7,8,9,10]
+sumOfEvenNo = 0
+
+for x in num:
+    if x > 0 and x % 2 == 0:
+        sumOfEvenNo+=x
+print(sumOfEvenNo)    
