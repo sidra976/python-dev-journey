9d1b6b9c3548f905baa79268c9ef20109b8b14e9
sidra976 <sidraahmed4477@gmail.com>
2025-06-09 22:04:48 +0500

    add

--- 01_basics/loops.py
+++ 01_basics/loops.py
@@ -1,3 +1,12 @@
 list = ['mango' , 'apple' , 'peach']
 for fruit in list:
     print(fruit)
+
+numbers = [0,-1,2,3,-4,-6,-9]
+count_positive_int = 0
+
+for x in numbers:
+    if x > 0:
+        count_positive_int = count_positive_int+1
+
+print(count_positive_int)        
