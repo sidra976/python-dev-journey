6d09a6e66a355b9093d6b9ab0c2e37b0f69ded08
sidra976 <sidraahmed4477@gmail.com>
2025-06-10 20:27:55 +0500

    add

--- 01_basics/functions.py
+++ 01_basics/functions.py
@@ -1,7 +1,21 @@
 #question 1
 
 def square(num):
-    res = num ** 2
-    print(res)
+    return  num ** 2
+   
+res = square(3)    
+print(res)
 
-square(3)    
+#question 2
+def sum(a, b):
+    return a + b
+
+print(sum(1,2))
+
+#question 3
+import math
+
+def area(radius):
+    return math.pi * radius ** 2
+
+print(math.floor(area(3)))
