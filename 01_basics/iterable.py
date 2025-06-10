ebc7b9fa5b3d102eaf2ac81c4a12c99d5f70e232
sidra976 <sidraahmed4477@gmail.com>
2025-06-10 19:43:16 +0500

    add

---
+++ 01_basics/iterable.py
@@ -0,0 +1,8 @@
+import time
+city = 'newyork'
+age = 20
+
+list = [1,2,3,4,5]
+
+print(city)
+print(age)
--- 01_basics/loops.py
+++ 01_basics/loops.py
@@ -47,6 +47,50 @@ while i > 0:
     result *= i
     i -= 1
 print("Factorial of", num, "is", result)
+
+while True:
+    userNum = int(input('Enter a num between 1 to 10: '))
+    if 1<= userNum <= 10:
+        print("Correct Input")
+        break
+    else:
+        print("Invalid Input, try again: ")
        
-   
+num = 5
+is_prime = True
+
+if num < 2:
+    is_prime = False
+else:
+    for i in range(2, num):
+        if (num % i) == 0:
+            is_prime = False
+            break
+
+print(num, "is a prime number" if is_prime else "is not a prime number")
+
+items = ["apple", "banana", "orange", "apple", "mango"]
+
+unique_item = set()
+
+for item in items:
+    if item in unique_item:
+        print("Duplicate: ", item)
+        break
+    unique_item.add(item)
+
+
+import time
+
+wait_time = 1
+max_retries = 5
+attempts = 0
+
+while attempts < max_retries:
+    print("Attempt", attempts + 1, "- wait time", wait_time)
+    time.sleep(wait_time)
+    wait_time *= 2
+    attempts += 1
+
+
       
