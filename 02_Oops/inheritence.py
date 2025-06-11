ddec70e23c710ba92cc3ca92a09d1070f030f69e
sidra976 <sidraahmed4477@gmail.com>
2025-06-11 14:39:19 +0500

    add

--- 02_Oops/inheritence.py
+++ 02_Oops/inheritence.py
@@ -5,12 +5,16 @@ class Car:
         self.model = model
     def full_name(self):
         return f"{self.brand} {self.model}"
+    
+class Electric_Car(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)   
+        self.battery_size = battery_size
+         
 # now we make objects with car names
-carObj1 = Car("McLaren", "720S")
-carObj2 = Car("Rolls Royce", "Phantom")
 
-print(carObj1.brand)
+carObj1 = Electric_Car("Tesla", "Model S", 100)
+
 print(carObj1.model)
 print(carObj1.full_name())
-print(carObj2.brand)
-print(carObj2.model)
+
