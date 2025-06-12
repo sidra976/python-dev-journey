0483288509ab0d25a28879732beed8587eebe0fa
sidra976 <sidraahmed4477@gmail.com>
2025-06-12 20:56:07 +0500

    add

--- 05-projects/youtube-manager-4.py
+++ 05-projects/youtube-manager-4.py
@@ -1,25 +1,29 @@
-import json #import json
+import json
 
-def load_data(): 
+def load_data():
     try:
-        with open ('youtube.txt' , 'r') as file:
-            return json.load(file)
-    #except: # its a way of handling exceptions we have diff types
-    except FileNotFoundError: #we r using this one 'filenotfounderror'
+        with open('youtube.txt', 'r') as file:
+            test = json.load(file)
+            # print(type(test))
+            return test
+    except FileNotFoundError:
         return []
+    
 
-def save_data_helper(videos): #make another helper method
+def save_data_helper(videos): 
     with open('youtube.txt', 'w') as file:
         json.dump(videos, file)
 
 
 def list_all_videos(videos):
-    #  enumerate(videos , start= 1) we store videos in enumerate because we need index wise
-    for index, in enumerate(videos , start= 1):
-        print(f"{index}.")
+    for index, video in enumerate(videos , start= 1):
+        print(f"{index}. {video['name']}, Duration: {video['time']} ")
 
 def add_video(videos):
-    pass
+     name = input("Enter video name: ")
+     time = input("Enter video time: ")
+     videos.append({'name': name, 'time': time})
+     save_data_helper(videos)
 
 def update_video(videos):
      pass
@@ -28,9 +32,7 @@ def delete_video(videos):
      pass
 
 def main():
-    
-    videos = load_data() 
-
+    videos = load_data()
     while True:
         print("\n Youtube Manager | choose an option ")
         print("1. List all youtube videos ")
@@ -39,21 +41,21 @@ def main():
         print("4. Delete a youtube video ")
         print("5. Exit the app ")
         choice = input("Enter your choice: ")
+        # print(videos)
 
         match choice:
             case '1':
-                list_all_videos(videos) 
+                list_all_videos(videos)
             case '2':
-                add_video(videos)        
+                add_video(videos)
             case '3':
-                update_video(videos)      
-            case '4': 
-                delete_video(videos)       
+                update_video(videos)
+            case '4':
+                delete_video(videos)
             case '5':
                 break
             case _:
                 print("Invalid Choice")
-
 
 if __name__ ==  "__main__":
     main() 
