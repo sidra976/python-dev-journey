6e7f608136f8dcaa5b8759010c72a46906026309
sidra976 <sidraahmed4477@gmail.com>
2025-06-15 15:05:52 +0500

    add

--- 06-practice-problems/dictionary_prob/1-frequency_counter.py
+++ 06-practice-problems/dictionary_prob/1-frequency_counter.py
@@ -1,6 +1,49 @@
+#problem 1
 animals = ['cat', 'dog', 'cat']
-count = 0
-for i in  animals:
-    i.count() = count
+frequency = {}
+for animal in animals:
+    if animal in frequency:
+     frequency[animal]+=1
+    else:
+       frequency[animal]=1
+print(frequency)
 
-print(count)
+#problem 2
+word = "butterfly"
+vowel_counter = {}
+
+for i in word:
+    if i in 'aeiou':
+        if i in vowel_counter:
+            vowel_counter[i] += 1
+        else:
+            vowel_counter[i] = 1
+
+print(vowel_counter)
+
+#problem 3
+num = 112223
+digit_counter = {}
+
+for i in str(num):
+   if i in digit_counter:
+      digit_counter[i] +=1
+   else:
+      digit_counter[i] =1
+
+print(digit_counter)
+
+#problem 4
+sentence = "hello world"
+
+letter_counter = {}
+for i in sentence:
+   if i == " ":
+      continue
+   if i in letter_counter:
+      letter_counter[i] +=1
+   else:
+      letter_counter[i] = 1
+
+print(letter_counter)      
+   
